2addcf316eeed908c0d94174d51ea46488088c4c
Jose Corbacho <JoseAntonio.Corbacho@sky.uk>
2018-02-01 13:36:38 +0000

    LinkedStack refactored to avoid castings

--- Lessons/src/main/kotlin/com/mccorby/algorithms/lesson2/LinkedStack.kt
+++ Lessons/src/main/kotlin/com/mccorby/algorithms/lesson2/LinkedStack.kt
@@ -2,34 +2,34 @@ package com.mccorby.algorithms.lesson2
 
 class LinkedStack<T>: Iterable<T> {
     private var size: Int = 0
-    private var first: Node = Node.Empty
+    private var first: Node<T> = Node.Empty
 
     fun push(item: T) {
         first = when (first) {
             Node.Empty -> {
                 Node.Item(item, Node.Empty)
             }
-            is Node.Item<*> -> {
+            is Node.Item<T> -> {
                 Node.Item(item, first)
             }
         }
         size++
     }
 
-    fun size(): Int {
-        return this.size
-    }
-
     fun pop(): T? {
         val aNode = first
         return when(aNode) {
             Node.Empty -> null
-            is Node.Item<*> -> {
+            is Node.Item<T> -> {
                 first = aNode.next
                 size--
-                aNode.item as T
+                aNode.item
             }
         }
+    }
+
+    fun size(): Int {
+        return this.size
     }
 
     fun isEmpty(): Boolean {
@@ -40,14 +40,14 @@ class LinkedStack<T>: Iterable<T> {
         return StackIterator(first)
     }
 
-    private sealed class Node {
-        object Empty: Node()
-        class Item<out T>(val item: T, val next: Node): Node()
+    private sealed class Node<out A> {
+        object Empty: Node<Nothing>()
+        class Item<out T>(val item: T, val next: Node<T>): Node<T>()
     }
 
-    private class StackIterator<out T>(first: Node): Iterator<T> {
+    private class StackIterator<out T>(first: Node<T>): Iterator<T> {
 
-        private var current: Node = first
+        private var current: Node<T> = first
 
         override fun hasNext(): Boolean {
             return current != Node.Empty
